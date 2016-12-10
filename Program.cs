989513525ca2578bb937030b29c504aaca2b5661
juliocefe <juliocesarfelixhz95@gmail.com>
2016-12-09 19:10:53 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,44 +10,27 @@ namespace CINE
 
 
 
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 			Console.WriteLine("Bienvenido a cinepolis");
 			Console.WriteLine("");
 
-			int boleto= 50, Aboletos=0, producto2=0;
+			int boleto = 50, Aboletos = 0, producto2 = 0;
 			string desea1, desea;
-			string QUESO="", DOGOGRANDE="";
+			string QUESO = "", DOGOGRANDE = "";
 			double venta = 0, Tboletos = 0, cboletos = 0;
 			int producto;
-			string pelicula="", tamaño="";
+			string pelicula = "", tamaño = "";
 			string P1, P2, P3, P4, P5;
 			double promo = 0;
 			double IVA;
-			int niños=0, mayores = 0; //boletos de niños  adulto
-			string niñYabues="";
-			int PAL=0, CHE=0, DUL=0, NAT=0; // PALOMITAS
-			int SOD=0, J=0, G=0, M=0, CH=0; // TAMANO DE SODAS
-			int NACH=0, NACHQ=0; // NACHOS
-			int DOG=0, DOGD=0; // TAMANOS DOGOS
-			int DULCES=0, GOM=0,CHO=0,PALE=0,CHIC=0,PUL=0; 
-			int PQ1=0, PQ2=0, PQ3=0; // PAQUETES 
-			int palomita=0, soda=0, dulce=0, paquete=0 ,comida=0; //TIPOS DE PRODUCTOS
+			int niños = 0, mayores = 0; //boletos de niños  adulto
+			string niñYabues = "";
+			int PAL = 0, CHE = 0, DUL = 0, NAT = 0; // PALOMITAS
+			int SOD = 0, J = 0, G = 0, M = 0, CH = 0; // TAMANO DE SODAS
+			int NACH = 0, NACHQ = 0; // NACHOS
+			int DOG = 0, DOGD = 0; // TAMANOS DOGOS
+			int DULCES = 0, GOM = 0, CHO = 0, PALE = 0, CHIC = 0, PUL = 0;
+			int PQ1 = 0, PQ2 = 0, PQ3 = 0; // PAQUETES 
+			int palomita = 0, soda = 0, dulce = 0, paquete = 0, comida = 0; //TIPOS DE PRODUCTOS
 
 
 
@@ -79,16 +62,16 @@ namespace CINE
 			}
 			Console.ReadKey();
 			Console.Clear();
-		
+
 
 
 			Console.WriteLine("                 MENU  ");
 			Console.WriteLine("");
-			Console.WriteLine(P1="[1]Blood in Blood out");
-			Console.WriteLine(P2="[2]El infierno");
-			Console.WriteLine(P3="[3]A la mas grande le puse cuca");
-			Console.WriteLine(P4="[4]AI MUCHAS COSAS WUUUUU");
-			Console.WriteLine(P5="[5]E.T");
+			Console.WriteLine(P1 = "[1]Blood in Blood out");
+			Console.WriteLine(P2 = "[2]El infierno");
+			Console.WriteLine(P3 = "[3]A la mas grande le puse cuca");
+			Console.WriteLine(P4 = "[4]AI MUCHAS COSAS WUUUUU");
+			Console.WriteLine(P5 = "[5]E.T");
 
 			Console.WriteLine("");
 			Console.WriteLine("Seleccione una pelicula");
@@ -99,7 +82,7 @@ namespace CINE
 
 
 
-			if (desea1 == "S" || desea1 == "s") 
+			if (desea1 == "S" || desea1 == "s")
 			{
 				//Console.WriteLine("         DULCERIA");
 				//Console.WriteLine("");
@@ -143,7 +126,7 @@ namespace CINE
 					Console.ReadKey();
 					Console.Clear();
 
-				
+
 					if (producto == 1) // SI ELEGISTE PALOMITAS
 					{
 						Console.WriteLine("            Escoge el tipo de palomita");
@@ -152,7 +135,7 @@ namespace CINE
 						Console.WriteLine("[2]Dulces................$70");
 						Console.WriteLine("[3]Naturales.............$75");
 						palomita = Convert.ToInt32(Console.ReadLine());
-						if (palomita == 1 )
+						if (palomita == 1)
 						{
 							venta += 85;
 							CHE = CHE + 1;
@@ -213,7 +196,7 @@ namespace CINE
 						{
 							Console.WriteLine("Desea su dogo grande? [S/N]");
 							DOGOGRANDE = Console.ReadLine();
-						
+
 							if (DOGOGRANDE == "S" || DOGOGRANDE == "s")
 							{
 								venta += 100;
@@ -243,7 +226,7 @@ namespace CINE
 						}
 
 					}
-				
+
 					if (producto == 4)
 					{
 						Console.WriteLine("QUE DULCE DECEA COMPRAR?");
@@ -252,7 +235,7 @@ namespace CINE
 						Console.WriteLine("[3]PALETAS");
 						Console.WriteLine("[4]CHICLES");
 						Console.WriteLine("[5]PULPARINDO");
-					
+
 						if (dulce == 1)
 						{
 							venta += 15;
@@ -284,7 +267,7 @@ namespace CINE
 							DULCES = DULCES + 1;
 
 						}
-					
+
 					}
 					if (producto == 5) // SI ELEGISTE PAQUETES
 					{
@@ -314,22 +297,22 @@ namespace CINE
 					if (pelicula == "P3")
 					{
 
-						if (producto == 2  & producto2 == 1)
+						if (producto == 2 & producto2 == 1)
 						{
 
-								Console.WriteLine("Tiene usted un descuento del 10% en su compra final :D");
-								promo += (75 + 70) * 0.10;
-								venta = (75 + 70) - promo;
+							Console.WriteLine("Tiene usted un descuento del 10% en su compra final :D");
+							promo += (75 + 70) * 0.10;
+							venta = (75 + 70) - promo;
 						}
 					}
-				
+
 					Console.WriteLine("Desea comprar algo mas?");
 					desea = Console.ReadLine();
-				
-					} while (desea == "Si" || desea == "si" || desea == "SI" || desea == "zi" || desea == "ci");
+
+				} while (desea == "Si" || desea == "si" || desea == "SI" || desea == "zi" || desea == "ci");
 
 
-		
+
 
 			}
 			if (pelicula == "P5" & mayores > 1)
@@ -344,22 +327,20 @@ namespace CINE
 			if (PQ2 > 0) { Console.WriteLine(PQ2 + " PAQUETES 2 VENDIDOS"); }
 			if (PQ3 > 0) { Console.WriteLine(PQ3 + " PAQUETES 3 VENDIDOS"); }
 			if (PAL > 0) { Console.WriteLine(PAL + " PALOMITAS VENDIDAS"); }
-			if (CHE > 0) { Console.WriteLine(CHE + " CHEDAR");}
+			if (CHE > 0) { Console.WriteLine(CHE + " CHEDAR"); }
 			if (DUL > 0) { Console.WriteLine(DUL + " DULCES"); }
-			if (NAT > 0) { Console.WriteLine("		" + NAT + "PALOMITAS NATURALES");}
-			if (SOD > 0) { Console.WriteLine(SOD + " SODAS VENDIDAS");}
+			if (NAT > 0) { Console.WriteLine("		" + NAT + "PALOMITAS NATURALES"); }
+			if (SOD > 0) { Console.WriteLine(SOD + " SODAS VENDIDAS"); }
 			if (J > 0) { Console.WriteLine(J + " SODAS JUMBO"); }
 			if (G > 0) { Console.WriteLine(G + " SODAS GRANDE"); }
-			if (M > 0) { Console.WriteLine(M + " SODAS  MEDIANA");}
-			if (CH > 0) { Console.WriteLine(CH + " SODAS CHICAS");}
-			if (DULCES> 0) { Console.WriteLine(DULCES + " DULCES VENDIDOS");}
+			if (M > 0) { Console.WriteLine(M + " SODAS  MEDIANA"); }
+			if (CH > 0) { Console.WriteLine(CH + " SODAS CHICAS"); }
+			if (DULCES > 0) { Console.WriteLine(DULCES + " DULCES VENDIDOS"); }
 			if (GOM > 0) { Console.WriteLine(GOM + " GOMITAS"); }
-			if (CHO > 0) { Console.WriteLine(CHO + " CHOCOLATES");}
-			if (PALE > 0) { Console.WriteLine(PALE + " PALETAS");}
-			if (CHIC > 0) { Console.WriteLine(CHIC + " CHICLES");}
-			if (PUL > 0) { Console.WriteLine(PUL + " PULPARINDO");}
-		
-
+			if (CHO > 0) { Console.WriteLine(CHO + " CHOCOLATES"); }
+			if (PALE > 0) { Console.WriteLine(PALE + " PALETAS"); }
+			if (CHIC > 0) { Console.WriteLine(CHIC + " CHICLES"); }
+			if (PUL > 0) { Console.WriteLine(PUL + " PULPARINDO"); }
 			if (cboletos > 0) { Console.WriteLine(cboletos + "BOLETOS"); }
 			if (PUL > 0) { Console.WriteLine(niños + "Boletos de niños $20(pz)"); }
 			if (Aboletos > 0) { Console.WriteLine(Aboletos + "Boletos de adulto $50(pz)"); }
@@ -369,10 +350,10 @@ namespace CINE
 			IVA = venta * 0.16;
 			venta += Tboletos;
 			venta += IVA;
-			if (Tboletos > 0) { Console.WriteLine("Precio de los boletos: $" + Tboletos);}
+			if (Tboletos > 0) { Console.WriteLine("Precio de los boletos: $" + Tboletos); }
 			Console.WriteLine("Subtotal: $" + venta);
 			Console.WriteLine("IVA: $" + IVA);
-			Console.WriteLine("Total a pagar: $" + venta);																																															
+			Console.WriteLine("Total a pagar: $" + venta);
 
 		}
 	}
